58aba5e01b18179910502f23d440b146049ba37a
ayush060992002 <ayush060992002@gmail.com>
2023-09-09 14:16:51 +0530

    Day 2

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+"""
+Sample test cases
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(calc.add(3, 8), 11)
